cb3aba5112154721af056d104ea9dee4a6a1440f
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-12-02 09:41:09 +0800

    chore: use tabWidth 2

--- src/build.ts
+++ src/build.ts
@@ -1,25 +1,34 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as prettier from 'prettier';
+
+function format(contents: string) {
+  return prettier.format(contents, {
+    filepath: '_.json',
+    tabWidth: 2,
+    useTabs: false,
+  });
+}
 
 async function build() {
-    const modules = await Promise.all([
-        import('./nx.schema'),
-        import('./workspace.schema'),
-    ]);
+  const modules = await Promise.all([
+    import('./nx.schema'),
+    import('./workspace.schema'),
+  ]);
 
-    const entries = modules.flatMap((mod) => {
-        return Object.entries(mod.default);
-    });
+  const entries = modules.flatMap((mod) => {
+    return Object.entries(mod.default);
+  });
 
-    await Promise.all(
-        entries.map(([name, schema]) =>
-            fs.promises.writeFile(
-                path.join(__dirname, '..', 'dist', name),
-                JSON.stringify(schema, null, 4),
-                { encoding: 'utf8' }
-            )
-        )
-    );
+  await Promise.all(
+    entries.map(([name, schema]) =>
+      fs.promises.writeFile(
+        path.join(__dirname, '..', 'dist', name),
+        format(JSON.stringify(schema)),
+        { encoding: 'utf8' }
+      )
+    )
+  );
 }
 
 build();
--- src/nx.schema.ts
+++ src/nx.schema.ts
@@ -1,101 +1,101 @@
 import type { JSONSchema7, JSONSchema7Definition } from 'json-schema';
 
 const implicitDependencyEntry: JSONSchema7Definition = {
-    type: 'object',
-    additionalProperties: {
-        oneOf: [
-            { enum: ['*'] },
-            { type: 'array', items: { type: 'string' } },
-            { $ref: '#/definitions/implicitJsonSubsetDependency' },
-        ],
-    },
+  type: 'object',
+  additionalProperties: {
+    oneOf: [
+      { enum: ['*'] },
+      { type: 'array', items: { type: 'string' } },
+      { $ref: '#/definitions/implicitJsonSubsetDependency' },
+    ],
+  },
 };
 
 const implicitJsonSubsetDependency: JSONSchema7Definition = {
-    type: 'object',
-    additionalProperties: {
-        oneOf: [
-            { enum: ['*'] },
-            { type: 'array', items: { type: 'array' } },
-            { $ref: '#/definitions/implicitJsonSubsetDependency' },
-        ],
-    },
+  type: 'object',
+  additionalProperties: {
+    oneOf: [
+      { enum: ['*'] },
+      { type: 'array', items: { type: 'array' } },
+      { $ref: '#/definitions/implicitJsonSubsetDependency' },
+    ],
+  },
 };
 
 const nxAffectedConfigDef: JSONSchema7Definition = {
-    type: 'object',
-    properties: {
-        defaultBase: {
-            type: 'string',
-            description: 'Default based branch used by affected commands.',
-        },
+  type: 'object',
+  properties: {
+    defaultBase: {
+      type: 'string',
+      description: 'Default based branch used by affected commands.',
     },
-    additionalProperties: false,
+  },
+  additionalProperties: false,
 };
 
 const nxJsonConfigDef: JSONSchema7Definition = {
-    type: 'object',
-    description: 'Nx.json configuration',
-    properties: {
-        implicitDependencies: {
-            $ref: '#/definitions/implicitDependencyEntry',
-        },
-        npmScope: {
-            type: 'string',
-        },
-        affected: {
-            $ref: '#/definitions/nxAffectedConfigDef',
-        },
-        projects: {
-            type: 'object',
-            additionalProperties: {
-                $ref: '#/definitions/nxJsonProjectConfigDef',
-            },
-        },
-        workspaceLayout: {
-            type: 'object',
-            properties: {
-                libsDir: { type: 'string' },
-                appsDir: { type: 'string' },
-            },
-            additionalProperties: false,
-        },
-        tasksRunnerOptions: {
-            type: 'object',
-            additionalProperties: {
-                type: 'object',
-                properties: {
-                    runner: { type: 'string' },
-                    options: { type: 'object', additionalProperties: true },
-                },
-                additionalProperties: false,
-                required: ['runner'],
-            },
-        },
+  type: 'object',
+  description: 'Nx.json configuration',
+  properties: {
+    implicitDependencies: {
+      $ref: '#/definitions/implicitDependencyEntry',
     },
-    required: ['implicitDependencies', 'npmScope', 'projects'],
+    npmScope: {
+      type: 'string',
+    },
+    affected: {
+      $ref: '#/definitions/nxAffectedConfigDef',
+    },
+    projects: {
+      type: 'object',
+      additionalProperties: {
+        $ref: '#/definitions/nxJsonProjectConfigDef',
+      },
+    },
+    workspaceLayout: {
+      type: 'object',
+      properties: {
+        libsDir: { type: 'string' },
+        appsDir: { type: 'string' },
+      },
+      additionalProperties: false,
+    },
+    tasksRunnerOptions: {
+      type: 'object',
+      additionalProperties: {
+        type: 'object',
+        properties: {
+          runner: { type: 'string' },
+          options: { type: 'object', additionalProperties: true },
+        },
+        additionalProperties: false,
+        required: ['runner'],
+      },
+    },
+  },
+  required: ['implicitDependencies', 'npmScope', 'projects'],
 };
 
 const nxJsonProjectConfigDef: JSONSchema7Definition = {
-    type: 'object',
-    properties: {
-        implicitDependencies: { type: 'array', items: { type: 'string' } },
-        tags: { type: 'array', items: { type: 'string' } },
-    },
-    additionalProperties: false,
+  type: 'object',
+  properties: {
+    implicitDependencies: { type: 'array', items: { type: 'string' } },
+    tags: { type: 'array', items: { type: 'string' } },
+  },
+  additionalProperties: false,
 };
 
 const nxJsonSchema: JSONSchema7 = {
-    $schema: 'http://json-schema.org/draft-07/schema#',
-    definitions: {
-        implicitDependencyEntry,
-        implicitJsonSubsetDependency,
-        nxAffectedConfigDef,
-        nxJsonConfigDef,
-        nxJsonProjectConfigDef,
-    },
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  definitions: {
+    implicitDependencyEntry,
+    implicitJsonSubsetDependency,
+    nxAffectedConfigDef,
+    nxJsonConfigDef,
+    nxJsonProjectConfigDef,
+  },
 };
 
 export default {
-    'nx.schema.json': nxJsonSchema,
+  'nx.schema.json': nxJsonSchema,
 };
